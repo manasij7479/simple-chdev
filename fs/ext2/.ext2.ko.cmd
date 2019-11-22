e18e67b15357c4681e256d955efea8c10424da66
Pranav Kant <pranavk@cs.utah.edu>
2019-11-21 18:19:59 -0700

    ext2 module

---
+++ fs/ext2/.ext2.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pranavk/advos/ext2/ext2.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/pranavk/advos/ext2/ext2.ko /home/pranavk/advos/ext2/ext2.o /home/pranavk/advos/ext2/ext2.mod.o ;  true
